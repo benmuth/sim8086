c298180591b3faa4957b35e1b0f771862ba4eccc
Ben Muthalaly <benmuthalaly@gmail.com>
2023-05-11 00:57:30 -0500

    Improve arg parsing logic.

--- sim8086.zig
+++ sim8086.zig
@@ -2,11 +2,14 @@ const std = @import("std");
 const print = @import("std").debug.print;
 
 pub fn main() void {
-    // std.debug.print("Hello world!\n", .{});
     var args = std.process.args();
-    // const arg0 = args.next().?[0];
-    while (true) {
-        var arg = args.next() orelse break;
-        print("{s}\n", .{arg});
+
+    // skip the name of the binary
+    _ = args.skip();
+    var file = args.next();
+    if (file) |name| { // payload capture
+        print("{s}\n", .{name});
+    } else {
+        print("give the file name as an argument\n", .{});
     }
 }
