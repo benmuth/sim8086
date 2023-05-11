dc29ba8534683297a49ec396101665a06cc50159
Ben Muthalaly <benmuthalaly@gmail.com>
2023-05-11 01:14:54 -0500

    Split file input logic into separate function.

--- sim8086.zig
+++ sim8086.zig
@@ -2,14 +2,26 @@ const std = @import("std");
 const print = @import("std").debug.print;
 
 pub fn main() void {
+    var file_name = getFileName() catch |err| {
+        if (err == FileError.NoFileGiven) {
+            print("no file given", .{});
+        }
+        return;
+    };
+    print("{s}\n", .{file_name});
+}
+
+fn getFileName() FileError![]const u8 {
     var args = std.process.args();
 
     // skip the name of the binary
     _ = args.skip();
     var file = args.next();
     if (file) |name| { // payload capture
-        print("{s}\n", .{name});
+        return name;
     } else {
-        print("give the file name as an argument\n", .{});
+        return FileError.NoFileGiven;
     }
 }
+
+const FileError = error{NoFileGiven};
