da8bc6c6bdba2a242403cf57cc13f56b05fe1e2e
Ben Muthalaly <benmuthalaly@gmail.com>
2023-05-12 15:30:23 -0500

    Get correct disasm printing to stdout.
    Works for mov instructions.

--- sim8086.zig
+++ sim8086.zig
@@ -1,20 +1,39 @@
 const std = @import("std");
 const print = @import("std").debug.print;
+const code = @import("encoding.zig");
 
-pub fn main() void {
-    var file_name = getFileName() catch |err| {
-        if (err == FileError.NoFileGiven) {
-            print("no file given", .{});
-        }
+const resource_dir = "/Users/ben/Documents/Programming/perf-aware/resources/part1";
+
+pub fn main() !void {
+    var fileName = getFileName() catch {
+        print("failed to get file name", .{});
         return;
     };
-    print("{s}\n", .{file_name});
+
+    var file = openFile(fileName) catch {
+        print("failed to open file with file name {s}", .{fileName});
+        return;
+    };
+    defer file.close();
+
+    const fs = try file.stat();
+
+    // print("{d}\n", .{fs.size});
+
+    const size = 100;
+
+    var buf: [size]u8 = undefined;
+    _ = try file.read(&buf);
+
+    var data = buf[0..fs.size];
+
+    code.decode(data);
 }
 
 fn getFileName() FileError![]const u8 {
     var args = std.process.args();
 
-    // skip the name of the binary
+    // skip the name of this binary
     _ = args.skip();
     var file = args.next();
     if (file) |name| { // payload capture
@@ -25,3 +44,18 @@ fn getFileName() FileError![]const u8 {
 }
 
 const FileError = error{NoFileGiven};
+
+fn openFile(fileName: []const u8) !std.fs.File {
+    var dir = std.fs.openDirAbsolute(resource_dir, .{ .access_sub_paths = true }) catch |err| {
+        print("failed to open dir", .{});
+        return err;
+    };
+
+    defer dir.close();
+
+    const file = dir.openFile(fileName, .{}) catch |err| {
+        print("failed to open file in resource dir\n", .{});
+        return err;
+    };
+    return file;
+}
